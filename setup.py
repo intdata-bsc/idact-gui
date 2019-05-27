92bead1148b333df48675f45ff8c97bc47fe275a
pswedrak <piotrekswedrak@gmail.com>
2019-05-27 18:09:03 +0200

    start gui

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="idact-gui",
+    version="0.0.1",
+    author="Piotr Swędrak",
+    author_email="piotrekswedrak@gmail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/intdata-bsc/idact",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
