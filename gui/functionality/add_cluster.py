5c7b8283d3ab8a4ef806fcf46b86ec067e61b897
Joanna Chyży <jnchyzy@gmail.com>
2019-09-03 23:34:51 +0200

    Add actions file chooser and use JupyterLab option

--- gui/functionality/add_cluster.py
+++ gui/functionality/add_cluster.py
@@ -1,11 +1,14 @@
 from PyQt5.QtWidgets import QLineEdit
 
 from gui.helpers.decorators import addToClass
+from PyQt5.QtWidgets import QFileDialog
+from idact.detail.config.client.setup_actions_config import SetupActionsConfigImpl
+from gui.decorators import addToClass
 from idact.core.auth import AuthMethod, KeyType
 from idact.core.add_cluster import add_cluster
 from idact import save_environment, load_environment
-from gui.functionality.idact_app import IdactApp, WindowType
-from idact.detail.log.get_logger import get_logger
+from gui.idact_app import IdactApp, WindowType
+from idact.detail.add_cluster_app import actions_parser as parser
 
 
 class AddCluster:
@@ -18,6 +21,9 @@ class AddCluster:
         idact_app.ui.port_edit.setValue(idact_app.parameters['add_cluster_arguments']['port'])
         idact_app.ui.auth_method_box.setCurrentText(idact_app.parameters['add_cluster_arguments']['authentication'])
         idact_app.ui.key_type_box.setCurrentText(idact_app.parameters['add_cluster_arguments']['key_type'])
+        idact_app.ui.add_actions_file_button.clicked.connect(idact_app.open_actions_file_dialog)
+
+        self.actions_file_name = None
 
     @addToClass(IdactApp)
     def add_cluster(self):
@@ -37,44 +43,47 @@ class AddCluster:
         self.saver.save(self.parameters)
         password = self.ui.password_edit.text()
 
-        log = get_logger(__name__)
+        use_jupyter_lab = self.ui.use_jupyter_lab_check_box.isChecked()
 
-        log.info("Loading environment...")
-        load_environment()
-
-        log.info("Adding cluster...")
+        setup_actions = None
+        if self.actions_file_name is not None:
+            setup_actions = SetupActionsConfigImpl()
+            setup_actions.jupyter = parser.parse_actions(self.actions_file_name)
 
         try:
             if auth == 'PUBLIC_KEY':
                 key = self.ui.key_type_box.currentText()
                 if key == 'RSA_KEY':
                     cluster = add_cluster(name=cluster_name,
-                                          user=user,
-                                          host=host,
-                                          port=port,
-                                          auth=AuthMethod.PUBLIC_KEY,
-                                          key=KeyType.RSA,
-                                          install_key=True)
-
+                                user=user,
+                                host=host,
+                                port=port,
+                                auth=AuthMethod.PUBLIC_KEY,
+                                key=KeyType.RSA,
+                                install_key=True,
+                                setup_actions=setup_actions,
+                                use_jupyter_lab=use_jupyter_lab)
             elif auth == 'ASK_EVERYTIME':
                 cluster = add_cluster(name=cluster_name,
-                                      user=user,
-                                      host=host,
-                                      port=port,
-                                      auth=AuthMethod.ASK)
+                            user=user,
+                            host=host,
+                            port=port,
+                            auth=AuthMethod.ASK,
+                            setup_actions=setup_actions,
+                            use_jupyter_lab=use_jupyter_lab)
 
-        except ValueError as e:
+                node = cluster.get_access_node()
+                node.connect(password=password)
+
+                save_environment()
+                self.popup_window.show_message("The cluster has been successfully added", WindowType.success)
+
+    except ValueError as e:
             self.popup_window.show_message("Cluster already exists", WindowType.error)
 
 
-        cluster.config.use_jupyter_lab = False
-        actions = ["module load plgrid/tools/python-intel/3.6.2"]
-        cluster.config.setup_actions.jupyter = actions
+    @addToClass(IdactApp)
+    def open_actions_file_dialog(self):
+        self.actions_file_name, _ = QFileDialog.getOpenFileName()
 
-        node = cluster.get_access_node()
-        node.connect(password=password)
-
-        save_environment()
-
-        self.popup_window.show_message("The cluster has been successfully added", WindowType.success)
 
