d305409078bdc92e3fab58293950a69211fb0a1d
PiekarzKarolina <piekarz.karolina@gmail.com>
2019-10-13 19:16:19 +0200

    Refactor remove-cluster

--- gui/functionality/remove_cluster.py
+++ gui/functionality/remove_cluster.py
@@ -1,45 +1,55 @@
-from gui.helpers.decorators import addToClass
-from gui.helpers.worker import Worker
+
+import os
+from PyQt5 import uic
+from PyQt5.QtWidgets import QVBoxLayout, QWidget
+
 from idact.core.remove_cluster import remove_cluster
 from idact import save_environment, load_environment
-from gui.functionality.idact_app import IdactApp, WindowType
+
+from gui.functionality.idact_app import WindowType
+from gui.helpers.worker import Worker
 
 
-class RemoveCluster:
-    def __init__(self, idact_app):
-        idact_app.ui.remove_cluster_button.clicked.connect(idact_app.concurrent_remove_cluster)
-        idact_app.ui.cluster_name_removec_edit.setText(idact_app.parameters['remove_cluster_arguments']['cluster_name'])
+class RemoveCluster(QWidget):
+    def __init__(self, parent=None):
+        QWidget.__init__(self, parent=parent)
+        self.parent = parent
+        ui_path = os.path.dirname(os.path.abspath(__file__))
+        self.ui = uic.loadUi(os.path.join(ui_path, '../widgets_templates/remove-cluster.ui'))
+        
+        self.ui.remove_cluster_button.clicked.connect(self.concurrent_remove_cluster)
+        self.ui.cluster_name_removec_edit.setText(self.parent.parameters['remove_cluster_arguments']['cluster_name'])
 
-    @addToClass(IdactApp)
+        lay = QVBoxLayout(self)
+        lay.addWidget(self.ui)
+     
     def concurrent_remove_cluster(self):
         worker = Worker(self.remove_cluster)
         worker.signals.result.connect(self.handle_complete_remove_cluster)
         worker.signals.error.connect(self.handle_error_remove_cluster)
-        self.threadpool.start(worker) 
+        self.parent.threadpool.start(worker) 
     
-    @addToClass(IdactApp)
     def handle_complete_remove_cluster(self):
         index = self.ui.cluster_names_box.findText(self.ui.cluster_name_removec_edit.text())
         self.ui.cluster_names_box.removeItem(index)
-        self.popup_window.show_message("The cluster has been successfully removed", WindowType.success)
+        self.parent.popup_window.show_message("The cluster has been successfully removed", WindowType.success)
     
-    @addToClass(IdactApp)
     def handle_error_remove_cluster(self, exception):
         if isinstance(exception, KeyError):
-            self.popup_window.show_message("The cluster does not exist", WindowType.error)
+            self.parent.popup_window.show_message("The cluster does not exist", WindowType.error)
         else:
-            self.popup_window.show_message("An error occured while removing cluster", WindowType.error)
+            self.parent.popup_window.show_message("An error occured while removing cluster", WindowType.error)
 
-    @addToClass(IdactApp)
     def remove_cluster(self):
         load_environment()
 
         cluster_name = self.ui.cluster_name_removec_edit.text()
-        self.parameters['remove_cluster_arguments']['cluster_name'] = cluster_name
-        self.saver.save(self.parameters)
+        self.parent.parameters['remove_cluster_arguments']['cluster_name'] = cluster_name
+        self.parent.saver.save(self.parameters)
         
         remove_cluster(cluster_name)
         save_environment()
         return
+
 
 
