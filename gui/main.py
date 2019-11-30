8d2a4ee076a691f8bf17eab7e0828787774c4654
PiekarzKarolina <piekarz.karolina@gmail.com>
2019-11-30 23:41:38 +0100

    Add args configuration provider

--- gui/main.py
+++ gui/main.py
@@ -10,6 +10,8 @@ def main():
     conf_provider = ConfigurationProvider()
     if not conf_provider.check_if_conf_file_exists():
         conf_provider.create_conf_file()
+    if not conf_provider.check_if_args_files_exist():
+        conf_provider.create_args_files()
     main_window = MainWindow()
     main_window.show()
     sys.exit(app.exec_())
